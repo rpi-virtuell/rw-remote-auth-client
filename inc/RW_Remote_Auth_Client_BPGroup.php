6771d74fdfbdcf6160ad3b526873945749f8a8ba
Joachim Happel <joachim.happel@gmail.com>
2016-03-08 09:54:51 +0100

    Konzept überarbeitet

--- inc/RW_Remote_Auth_Client_BPGroup.php
+++ inc/RW_Remote_Auth_Client_BPGroup.php
@@ -5,7 +5,7 @@
  *
  * adds a adminpage under the users page with
  *     Headline/Menu: Add Group Member
- *     1. URL Field (enter url to buddypress Group)
+ *     1. CODE field (base64 :    {group_id:'123',url:'http://....'} )
  *     2. List of successfull added group member
  *     3. Name of the group
  *     4. Button Add/Update
@@ -20,9 +20,9 @@
  *     1. group_id     (group slug may be not persistent)
  *     2. user_login of the blog admin
  *
- * exprect a json width a
+ * expect a json width a
  *    1. list of the member
- *    2. group details (slug,name,id,host)
+ *    2. group details (name,url)
  *    or
  *      response error messages (http error code) can be
  *      - admin is not a member (403)
@@ -30,10 +30,11 @@
  *      - invalid request (406)
  *      - no json (405)
  *
+
  * on success
  *    1. adds member of a external buddypress group to a blog
  *    2. adds the group (slug,name,id,host) to current blog options
- *
+ *    3. send a success:true request
  *
  * displays buddypress group on the dasboard for all member
  * displays the add group member site
@@ -48,28 +49,29 @@ class RW_Remote_Auth_Client_BPGroup
 
     /**
      * asking for a group
+     * @hash base64 returns json with:
+     *      @param $url       (to external bp group)
+     *      @param $group_id  (from external bp group)
+     * @param $admin     (from current_user)
      *
-     * @param $url
-     * @param $admin
+     * save the to the blog options ( rw_remote_auth_client_bpgroups[ host_group_id : {group_id:'123',url:'http://....'} ]
      *
-     * save the to the blog options ( rw_remote_auth_client_bpgroups )
+     * @return a stdClass with the group_details
      *
-     * @return a stdClass with the group_details ($host_url, group_id, group_name, guid)
+     *      group->info = stdClass
+     *          info->name
+     *          info->url
+     *
+     *      group->members = array{
+     *
+     *          stdClass Member:
+     *              Member->name
+     *              Member->url
+     *
+     *      )
+     *
      */
-    public static function get_group_info( $url, $admin ){
-
-    }
-
-    /**
-     * asking for the group member     *
-     *
-     * @param $host_url
-     * @param $group_id
-     * @param $admin
-     *
-     * @returns a array of group member (user_login)
-     */
-    public static function get_group_member ( $host_url, $group_id , $admin ){
+    public static function get_group( $hash, $admin ){
 
     }
 
